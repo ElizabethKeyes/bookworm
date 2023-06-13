7eacbb2b745ab7ebeea001ea40674152851b09fe
Elizabeth <liz4momentum@gmail.com>
2023-06-13 15:35:12 -0600

    Hardcover fiction drawing to page, needs styled

--- bookworm.client/src/pages/HomePage.vue
+++ bookworm.client/src/pages/HomePage.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="container-fluid">
-    <section class="row">
-
+    <section class="row justify-content-center">
+      <div class="col-10">
+        <h1>Bestsellers - Hardcover Fiction</h1>
+        <div v-for="b in hardcoverFictionList[0].books" :key="b.title" class="hardcoverFictionList">
+          <img :src="b.bookImage" :alt="'a photo of ' + b.title">
+        </div>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
-import { onMounted } from "vue";
+import { computed, onMounted } from "vue";
 import { logger } from "../utils/Logger.js";
 import { nytListsService } from "../services/NytListsService.js"
+import { AppState } from "../AppState.js";
 
 export default {
   setup() {
@@ -24,7 +30,9 @@ export default {
     onMounted(() => {
       getNytLists()
     })
-    return {}
+    return {
+      hardcoverFictionList: computed(() => AppState.NytLists.filter(l => l.listId == 1))
+    }
   }
 }
 </script>
