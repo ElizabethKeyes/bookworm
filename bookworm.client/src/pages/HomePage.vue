d81c921f7c93d0b50ce8609058103bcbbeb31fff
Elizabeth <liz4momentum@gmail.com>
2023-06-13 15:46:42 -0600

    Adjusted margin

--- bookworm.client/src/pages/HomePage.vue
+++ bookworm.client/src/pages/HomePage.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <section class="row justify-content-center">
       <div class="col-10">
-        <h1>Bestsellers - Hardcover Fiction</h1>
+        <h1 class="mt-2">Bestsellers - Hardcover Fiction</h1>
         <div class="hardcoverFictionList">
           <div v-for="b in hardcoverFictionList[0].books" :key="b.title" class="books">
             <img :src="b.bookImage" :alt="'a photo of ' + b.title">
